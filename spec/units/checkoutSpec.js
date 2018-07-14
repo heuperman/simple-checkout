e77a8525c47d3857260365009d449a2d3f4372e4
Cornelis Heuperman <cheuperman@gmail.com>
2018-07-14 11:45:28 +0100

    Remove private method tests

--- spec/units/checkoutSpec.js
+++ spec/units/checkoutSpec.js
@@ -20,17 +20,4 @@ beforeEach(function() {
       expect(checkout.total()).toEqual("£8.97");
     });
   });
-
-  describe("_formatPrice", function() {
-    it("returns a number as string including a £ symbol", function() {
-      expect(checkout._formatPrice(2.99)).toEqual("£2.99");
-    });
-  });
-
-  describe("_addPrices", function() {
-    it("returns the total price of all scanned items", function() {
-      for (i = 0; i < 3; i++) { checkout.scan(sample); }
-      expect(checkout._addPrices()).toEqual(8.97);
-    });
-  });
 });
